8a11d834ee2b12a182fded442f19c01012eeeb0c
CarlosZoft <carlosoftwar@gmail.com>
2022-01-16 10:01:27 -0300

    test: remove side effects✔️

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,8 +1,10 @@
 import { Dollar } from "./Dollar";
 describe("Dollar tests", () => {
   it("Should return 10 when five dollars are multiplied by two", () => {
-    const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toBe(10);
+    const five: Dollar = new Dollar(5);
+    var product: Dollar = five.times(2);
+    expect(product.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });
