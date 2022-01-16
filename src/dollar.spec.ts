e75d31953a956ff1e039039b5e0a1b1664260b79
CarlosZoft <carlosoftwar@gmail.com>
2022-01-16 10:18:59 -0300

    test: dollar make amount private ✔️

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,11 +1,9 @@
 import { Dollar } from "./Dollar";
 describe("Dollar tests", () => {
-  it("Should return 10 when five dollars are multiplied by two", () => {
+  it("Should return 10 when five dollars are multiplied by two/three", () => {
     const five: Dollar = new Dollar(5);
-    var product: Dollar = five.times(2);
-    expect(product.amount).toBe(10);
-    product = five.times(3);
-    expect(product.amount).toBe(15);
+    expect(five.times(2).equals(new Dollar(10))).toBe(true);
+    expect(five.times(3).equals(new Dollar(15))).toBe(true);
   });
 
   it("should return true when $5 is compared to %5", () => {
