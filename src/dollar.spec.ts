df3dedddb26be9e3795bed80546f8dc724bd3ddd
CarlosZoft <carlosoftwar@gmail.com>
2022-01-17 20:58:49 -0300

    ✅  test: base euro class

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,9 +1,14 @@
 import { Dollar } from "./Dollar";
-describe("Dollar tests", () => {
-  it("Should return 10 when five dollars are multiplied by two/three", () => {
+import { Euro } from "./Euro";
     const five: Dollar = new Dollar(5);
     expect(five.times(2).equals(new Dollar(10))).toBe(true);
     expect(five.times(3).equals(new Dollar(15))).toBe(true);
+  });
+
+  it("Should return 10 when five euros are multiplied by two and 15 when multiplied by three", () => {
+    const five: Euro = new Euro(5);
+    expect(five.times(2).equals(new Euro(10))).toBe(true);
+    expect(five.times(3).equals(new Euro(15))).toBe(true);
   });
 
   it("should return true when $5 is compared to %5", () => {
