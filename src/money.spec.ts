c37c6a326afbc1e849441dfdf8f010fad0577999
CarlosZoft <carlosoftwar@gmail.com>
2022-01-17 22:52:22 -0300

    ♻️ refactor : exchange description of tests

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -2,22 +2,22 @@ import { Dollar } from "./Dollar";
 import { Euro } from "./Euro";
 
 describe("Money", () => {
-  it("Should return 10 when five dollars are multiplied by two and 15 when multiplied by three", () => {
+  it("Should correctly handle dollar multiplication", () => {
     const five: Dollar = new Dollar(5);
-    expect(five.times(2).equals(new Dollar(10))).toBe(true);
-    expect(five.times(3).equals(new Dollar(15))).toBe(true);
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
   });
 
-  it("Should return 10 when five euros are multiplied by two and 15 when multiplied by three", () => {
+  it("Should correctly handle euro multiplication", () => {
     const five: Euro = new Euro(5);
-    expect(five.times(2).equals(new Euro(10))).toBe(true);
-    expect(five.times(3).equals(new Euro(15))).toBe(true);
+    expect(five.times(2).equals(new Euro(10))).toBeTruthy();
+    expect(five.times(3).equals(new Euro(15))).toBeTruthy();
   });
 
-  it("should return true when dollars and eutos of the same value are compared", () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-    expect(new Euro(5).equals(new Euro(5))).toBe(true);
-    expect(new Euro(5).equals(new Euro(6))).toBe(false);
+  it("Should correctly handle money equality", () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
+    expect(new Euro(5).equals(new Euro(6))).toBeFalsy();
   });
 });
