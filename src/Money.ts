c9f8698d6df26e935d8dcb97a6a26a63def208f3
CarlosZoft <carlosoftwar@gmail.com>
2022-01-22 21:39:35 -0300

    ✨ feat: created factory methods for dollars and euros

--- src/Money.ts
+++ src/Money.ts
@@ -1,10 +1,38 @@
-export class Money {
+export abstract class Money {
   protected readonly amount: number;
   constructor(amount: number) {
     this.amount = amount;
   }
+  static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
+
+  static euro(amount: number): Euro {
+    return new Euro(amount);
+  }
 
   equals(otherMoney: Money): boolean {
     return this.amount === otherMoney.amount;
+  }
+
+  abstract times(multiplier: number): Money;
+}
+export class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+export class Euro extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Euro {
+    return new Euro(this.amount * multiplier);
   }
 }
