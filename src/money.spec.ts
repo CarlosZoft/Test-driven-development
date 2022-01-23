1dedbfa54331ea4c5e78aa3d95ea3f31a3602929
CarlosZoft <carlosoftwar@gmail.com>
2022-01-22 21:39:52 -0300

    ✅test : created factory methods for dollars and euros

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,24 +1,23 @@
-import { Dollar } from "./Dollar";
-import { Euro } from "./Euro";
+import { Money } from "./Money";
 
 describe("Money", () => {
   it("Should correctly handle dollar multiplication", () => {
-    const five: Dollar = new Dollar(5);
-    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
+    const five: Money = Money.dollar(5);
+    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
   });
 
   it("Should correctly handle euro multiplication", () => {
-    const five: Euro = new Euro(5);
-    expect(five.times(2).equals(new Euro(10))).toBeTruthy();
-    expect(five.times(3).equals(new Euro(15))).toBeTruthy();
+    const five: Money = Money.euro(5);
+    expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
+    expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
   });
 
   it("Should correctly handle money equality", () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
-    expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
-    expect(new Euro(5).equals(new Euro(6))).toBeFalsy();
-    expect(new Euro(5).equals(new Dollar(6))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
+    expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy();
+    expect(Money.euro(5).equals(Money.dollar(6))).toBeFalsy();
   });
 });
