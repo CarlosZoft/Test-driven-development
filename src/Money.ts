09ebc36720d7447138ae2b1241856a51707f791f
CarlosZoft <carlosoftwar@gmail.com>
2022-01-23 23:44:20 -0300

    ✨ feat : currency and commom constructor

--- src/Money.ts
+++ src/Money.ts
@@ -1,38 +1,37 @@
 export abstract class Money {
-  protected readonly amount: number;
-  constructor(amount: number) {
+  protected _currency: string;
+  protected amount: number;
+
+  constructor(amount: number, currency: string) {
     this.amount = amount;
+    this._currency = currency;
   }
+
   static dollar(amount: number): Dollar {
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   static euro(amount: number): Euro {
-    return new Euro(amount);
+    return new Euro(amount, "EUR");
   }
 
   equals(otherMoney: Money): boolean {
     return this.amount === otherMoney.amount;
   }
 
+  currency(): string {
+    return this._currency;
+  }
   abstract times(multiplier: number): Money;
 }
 export class Dollar extends Money {
-  constructor(amount: number) {
-    super(amount);
-  }
-
   times(multiplier: number): Dollar {
-    return new Dollar(this.amount * multiplier);
+    return new Dollar(this.amount * multiplier, this._currency);
   }
 }
 
 export class Euro extends Money {
-  constructor(amount: number) {
-    super(amount);
-  }
-
   times(multiplier: number): Euro {
-    return new Euro(this.amount * multiplier);
+    return new Euro(this.amount * multiplier, this._currency);
   }
 }
