f18d8a6e5128e99aa9a9860319efc4f6606da2dc
CarlosZoft <carlosoftwar@gmail.com>
2022-01-24 23:00:52 -0300

    ✅ test : make times a money method

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -19,6 +19,7 @@ describe("Money", () => {
     expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
     expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy();
     expect(Money.euro(5).equals(Money.dollar(6))).toBeFalsy();
+    expect(new Money(5, "USD").equals(Money.dollar(5))).toBeTruthy();
   });
   it("Should correcly handle currencies", () => {
     expect(Money.dollar(1).currency()).toEqual("USD");
