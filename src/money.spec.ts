69ade2f7297bfb54e82f6450f63133e912d3e707
CarlosZoft <carlosoftwar@gmail.com>
2022-01-24 23:05:03 -0300

    ✅ test : remove redundant tests

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -7,20 +7,12 @@ describe("Money", () => {
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
   });
 
-  it("Should correctly handle euro multiplication", () => {
-    const five: Money = Money.euro(5);
-    expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
-  });
-
   it("Should correctly handle money equality", () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
-    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
-    expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy();
     expect(Money.euro(5).equals(Money.dollar(6))).toBeFalsy();
-    expect(new Money(5, "USD").equals(Money.dollar(5))).toBeTruthy();
   });
+
   it("Should correcly handle currencies", () => {
     expect(Money.dollar(1).currency()).toEqual("USD");
     expect(Money.euro(1).currency()).toEqual("EUR");
