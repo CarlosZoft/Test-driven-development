d2bb4a93df1b714181003fe12fe3d11b37359fbe
carlos.matos <carlos.matos@layers.education>
2022-01-26 00:03:50 -0300

    ♻️ refactor: improving test description

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,19 +1,19 @@
 import { Money } from "./Money";
 
 describe("Money", () => {
-  it("Should correctly handle dollar multiplication", () => {
+  it("Should handle dollar multiplication", () => {
     const five: Money = Money.dollar(5);
     expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
   });
 
-  it("Should correctly handle money equality", () => {
+  it("Should handle money equality", () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
     expect(Money.euro(5).equals(Money.dollar(6))).toBeFalsy();
   });
 
-  it("Should correcly handle currencies", () => {
+  it("Should handle currencies", () => {
     expect(Money.dollar(1).currency()).toEqual("USD");
     expect(Money.euro(1).currency()).toEqual("EUR");
   });
