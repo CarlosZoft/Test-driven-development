217809b3a8bad06b5188a2282104788a8dbf3e8f
Carlos Zoft <carlosk2gunn@hotmail.com>
2022-01-29 01:16:33 -0300

    ✅ test: reduce from a sum and from a money

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,5 +1,5 @@
 import { Money } from "./Money";
-import { Expression } from './expression'
+import { Expression } from "./expression";
 import { Bank } from "./Bank";
 import { Sum } from "./Sum";
 
@@ -22,23 +22,31 @@ describe("Money", () => {
   });
 
   it("Should handle simple addition", () => {
-    const five: Money = Money.dollar(5)
-    const sum: Expression = five.plus(five)
+    const five: Money = Money.dollar(5);
+    const sum: Expression = five.plus(five);
     const bank: Bank = new Bank();
-    const reduced: Money = bank.reduce(sum, 'USD')
-    expect(reduced.equals(Money.dollar(10))).toBeTruthy()
-  })
-  it('Should return a sum when plus is used', () => {
-    const five = Money.dollar(5)
-    const result:Expression = five.plus(five) 
+    const reduced: Money = bank.reduce(sum, "USD");
+    expect(reduced.equals(Money.dollar(10))).toBeTruthy();
+  });
+
+  it("Should return a sum when plus is used", () => {
+    const five = Money.dollar(5);
+    const result: Expression = five.plus(five);
     const sum = result as Sum;
-    expect(sum.augend).toEqual(five)
-    expect(sum.addend).toEqual(five)
-  })
-  it('should reduce a sum', () => {
-    const sum: Expression = new Sum(Money.dollar(3),Money.dollar(4))
+    expect(sum.augend).toEqual(five);
+    expect(sum.addend).toEqual(five);
+  });
+
+  it("should reduce from a sum", () => {
+    const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4));
     const bank = new Bank();
-    const result = bank.reduce(sum, 'USD')
-    expect(result).toEqual(Money.dollar(7))
-  })
+    const result = bank.reduce(sum, "USD");
+    expect(result).toEqual(Money.dollar(7));
+  });
+
+  it("should reduce from a money", () => {
+    const bank = new Bank();
+    const result = bank.reduce(Money.dollar(1), "EUR");
+    expect(result).toEqual(Money.dollar(1));
+  });
 });
