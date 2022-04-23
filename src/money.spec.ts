da0a8c7be1ca14aace386fa98cc2e401c7b3b4d8
CarlosZoft <carlosk2gunn@hotmail.com>
2022-04-23 16:16:32 -0300

    ✅ test: get closer to abstraction

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -6,8 +6,8 @@ import { Sum } from "./Sum";
 describe("Money", () => {
   it("Should handle dollar multiplication", () => {
     const five: Money = Money.dollar(5);
-    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
+    expect(five.times(2)).toEqual(Money.dollar(10));
+    expect(five.times(3)).toEqual(Money.dollar(15));
   });
 
   it("Should handle money equality", () => {
@@ -59,5 +59,14 @@ describe("Money", () => {
 
   it("should handle identity rate", () => {
     expect(new Bank().rate("USD", "USD")).toEqual(1);
+  });
+
+  it("Should handle mixed addition", () => {
+    const fiveBucks: Expression = Money.dollar(5);
+    const tenEuros: Expression = Money.euro(10);
+    const bank = new Bank();
+    bank.addRate("EUR", "USD", 2);
+    const result: Expression = bank.reduce(fiveBucks.plus(tenEuros), "USD");
+    expect(result).toEqual(Money.dollar(10));
   });
 });
